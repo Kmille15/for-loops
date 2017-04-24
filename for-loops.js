cce42ff2e7c00d7560276e0818fdcd9fbe3b5c47
KJ Miller <KJ Miller>
2017-04-24 15:26:15 -0500

    js file

---
+++ for-loops.js
@@ -0,0 +1,14 @@
+for (i=5; i<=120; i +=10) {console.log(i + "");}
+
+
+for (i=4096; i>=1; i /=2) {console.log(i + "");}
+
+
+var presidents = ["George Washington","John Adams","Thomas Jefferson"];
+var text= ["President #","President #", "President #"]
+var i;
+for (i=0; i<=2; i ++) {
+
+	console.log( text[i]  + (i+1) +" was "+presidents[i] ) ;}
+
+
